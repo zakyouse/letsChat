76e9858862818d32689073ce28086893eed3ae58
zakyouse <zedehcykei@gmail.com>
2024-03-13 12:50:26 +0300

    improved some features and added time stamp

--- src/components/Message.jsx
+++ src/components/Message.jsx
@@ -3,22 +3,35 @@ import { auth } from "../firebase";
 import { useAuthState } from "react-firebase-hooks/auth";
 function Message(message) {
   const [user] = useAuthState(auth);
+  const timestamp = message.message.createdAt;
+  const jsDate = new Date(
+    timestamp.seconds * 1000 + timestamp.nanoseconds / 1000000
+  );
+  const formattedLocalTime = jsDate.toLocaleTimeString([], {
+    hour: "2-digit",
+    minute: "2-digit",
+  });
   return (
     <div
       className={`chat-bubble ${
         message.message.uid === user.uid ? "right" : ""
       }`}
     >
+      <img
+        className={`${
+          message.message.uid === user.uid ? "right--img" : "left--img"
+        }`}
+        src={message.message.avatar}
+        alt="user avatar"
+      />
       <div className="chat-bubble-title">
-        <img
-          className="chat-bubble__left"
-          src={message.message.avatar}
-          alt="user avatar"
-        />
         <p className="user-name">{message.message.name}</p>
       </div>
       <div className="chat-bubble__right">
         <p className="user-message">{message.message.text}</p>
+      </div>
+      <div className="time">
+        <p className="time--text">{formattedLocalTime}</p>
       </div>
     </div>
   );
